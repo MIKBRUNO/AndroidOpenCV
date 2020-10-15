8eae455ea911960ea8166a3526a3435d816229ed
MIKBRUNO <mikdyach@mail.ru>
2020-10-15 16:34:24 +0700

    Added Frontal Face Detection

--- app/src/main/java/com/example/test_opencv/MainActivity.java
+++ app/src/main/java/com/example/test_opencv/MainActivity.java
@@ -56,24 +56,68 @@ package com.example.test_opencv;
 //}
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 
+import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Point;
-import org.opencv.core.Size;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MainActivity extends Activity implements CvCameraViewListener2 {
     private CameraBridgeViewBase mOpenCvCameraView;
+
+    private BaseLoaderCallback LoaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS:
+                    try {
+                        InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt);
+
+                        File CascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        CascadeFile = new File(CascadeDir, "haarcascade_frontalface_alt.xml");
+
+                        FileOutputStream fos = new FileOutputStream(CascadeFile);
+
+                        byte[] bytesBuffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = is.read(bytesBuffer)) != -1) {
+                            fos.write(bytesBuffer, 0, bytesRead);
+                        }
+                        is.close();
+                        fos.close();
+
+                        faceClassifier = new CascadeClassifier(CascadeFile.getAbsolutePath());
+                        CascadeDir.delete();
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,9 +144,13 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     public void onResume()
     {
         super.onResume();
-        OpenCVLoader.initDebug();
         mOpenCvCameraView.enableView();
         mOpenCvCameraView.enableFpsMeter();
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, this, LoaderCallBack);
+        } else {
+            LoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
     }
 
     public void onDestroy() {
@@ -117,23 +165,32 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     public void onCameraViewStopped() {
     }
 
-//    private CascadeClassifier face_classifier = new CascadeClassifier("");
+    private File CascadeFile;
+
+    private CascadeClassifier faceClassifier;
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-        double width = inputFrame.rgba().width();
-        double height = inputFrame.rgba().height();
-        Mat dst = new Mat();
-        Mat affineTransform = Imgproc.getAffineTransform(
-                new MatOfPoint2f(
-                        new Point(0, 0),
-                        new Point(1, 0),
-                        new Point(0, 1)),
-                new MatOfPoint2f(
-                        new Point(0, 0),
-                        new Point(1, 0.5),
-                        new Point(0, 1)
-                ));
-        Imgproc.warpAffine(inputFrame.rgba(), dst, affineTransform, new Size(width, height));
-        return dst;
+        Mat mGray = inputFrame.rgba();
+        MatOfRect mFoundRects = new MatOfRect();
+        faceClassifier.detectMultiScale(mGray, mFoundRects, 1.1, 2);
+        Rect[] rects = mFoundRects.toArray();
+        for (Rect rect : rects) {
+            Imgproc.rectangle(mGray, rect.tl(), rect.br(), new Scalar(0, 255, 0, 255));
+        }
+        return mGray;
+//        double width = inputFrame.rgba().width();
+//        double height = inputFrame.rgba().height();
+//        Mat dst = new Mat();
+//        Mat affineTransform = Imgproc.getAffineTransform(
+//                new MatOfPoint2f(
+//                        new Point(0, 0),
+//                        new Point(1, 0),
+//                        new Point(0, 1)),
+//                new MatOfPoint2f(
+//                        new Point(0, 0),
+//                        new Point(1, 0.5),
+//                        new Point(0, 1)
+//                ));
+//        Imgproc.warpAffine(inputFrame.rgba(), dst, affineTransform, new Size(width, height));
     }
 }
